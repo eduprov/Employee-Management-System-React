ca9f7fc487d255ee61c8205200c6eec8f27676f7
thephotogenicbug <navinkumarachari@gmail.com>
2021-08-17 15:16:27 +0530

    Fix Dashboard

---
+++ src/Components/test.js
@@ -0,0 +1,69 @@
+<div className="container ">
+            {/* <div className="row">
+               <div className="col-md-2 ">
+             
+               </div>
+               <Link to="/myprofile" className="col-md-3 mt-5">
+               <img src={man} className="profile-pic" />  
+               </Link>
+               <div className="col-md-4">
+               
+               </div>
+               <div className="col-md-3 power-button">
+                 
+
+                <Link className="logout" onClick={logout}>
+                  <li className="fa fa-power-off text-danger"></li> <span className="text-danger">Logout</span>
+            
+              </Link>
+               </div>
+            </div> */}
+            {/* <div className="row">
+                <div className="col-md-2">
+               
+                </div>
+             
+                <div className="col-lg-3">
+                <div className="card dashboard-cards interested">
+                        <div className="card-body">
+                            <h3 className="card-numbers"></h3> <i class="fa fa-paper-plane icons"></i>
+                            <h4 className="card-title">Apply For Leave</h4>
+                            <Link  to='/applyforleave'><i class="fas fa-arrow-circle-right text-white fa-2x"></i></Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3">
+                <div className="card dashboard-cards total-admissions">
+                        <div className="card-body">
+                            <h3 className="card-numbers"></h3> <i class="fas fa-rupee-sign icons"></i>
+                            <h4 className="card-title">Payslip</h4>
+                            <Link ><i class="fas fa-arrow-circle-right text-white fa-2x"></i></Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3">
+                <div className="card dashboard-cards dues-followup">
+                        <div className="card-body">
+                            <h3 className="card-numbers"></h3> <i class="fas fa-ticket-alt icons"></i>
+                            <h4 className="card-title">Raise Support</h4>
+                            <Link ><i class="fas fa-arrow-circle-right text-white fa-2x"></i></Link>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+            {/* <div className="row">
+                <div className="col-lg-2"></div>
+              
+                <div className="col-lg-3">
+                <div className="card dashboard-cards profile-update">
+                        <div className="card-body">
+                            <h3 className="card-numbers"></h3> <i class="far fa-user icons"></i>
+                            <h4 className="card-title">Update Profile</h4>
+                            <Link to="/signup" ><i class="fas fa-arrow-circle-right text-white fa-2x"></i></Link>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="col-lg-3"></div>
+            </div> */}
+        </div>
